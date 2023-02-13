b57e137cad827862f00b9964225a2f9b0fcd358f
Shilpriya <shilpriya.s@simformsolutions.com>
2023-02-13 11:43:05 +0530

    initial setup

---
+++ Git/Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Mansi Prajapati on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
